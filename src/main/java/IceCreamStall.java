9e77f9ef1b778ac99ab9f8b90560e046840166f3
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-02 17:18:50 +0100

    creates ice cream stall class

---
+++ src/main/java/IceCreamStall.java
@@ -0,0 +1,11 @@
+public class IceCreamStall extends Stall {
+
+    private String name;
+    private String ownerName;
+    private int parkingSpot;
+
+    public IceCreamStall(String name, String ownerName, int parkingSpot) {
+        super(name, ownerName, parkingSpot);
+    }
+
+}
---
+++ src/test/java/IceCreamStallTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IceCreamStallTest {
+
+    IceCreamStall iceCreamStall;
+
+    @Before
+    public void before() {
+        iceCreamStall = new IceCreamStall("Manx Ices", "Mr Davison", 001);
+    }
+
+    @Test
+    public void canGetAttributes() {
+        assertEquals("Manx Ices", iceCreamStall.getName());
+        assertEquals("Mr Davison", iceCreamStall.getOwnerName());
+        assertEquals(001, iceCreamStall.getParkingSpot());
+    }
+
+
+
+}
