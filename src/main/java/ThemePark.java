8447551b2f1121665b2110dc98742160b707139d
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-02 23:14:08 +0100

    can get all reviewed

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -7,7 +7,7 @@ public class ThemePark {
     private ArrayList<Stall> stalls;
     private ArrayList<Visitor> visitors;
 
-    public ThemePark(String name) {
+    public ThemePark(String name, ArrayList<Attraction> attractions, ArrayList<Stall> stalls, ArrayList<Visitor> visitors) {
         this.name = name;
         this.attractions = attractions;
         this.stalls = stalls;
@@ -17,7 +17,28 @@ public class ThemePark {
     public String getName() {
         return name;
     }
+    public ArrayList<Attraction> getAttractions() { return attractions; }
+    public ArrayList<Stall> getStalls() { return stalls; }
+    public ArrayList<Visitor> getVisitors() { return visitors; }
 
+    public int getAttractionsCount() { return attractions.size(); }
+    public int getStallsCount() { return stalls.size(); }
+    public int getVisitorsCount() { return visitors.size(); }
 
+    public ArrayList<IReviewed> getAllReviewed() {
+        ArrayList<IReviewed> allReviewed = new ArrayList<>();
+        for (IReviewed attraction : attractions) {
+            allReviewed.add(attraction);
+        }
+        for (IReviewed stall : stalls) {
+            allReviewed.add(stall);
+        }
+        return allReviewed;
+    }
+
+//    public String visit(Visitor visitor, Attraction attraction) {
+//        return "enjoying ride";
+//        }
+//    }
 
 }
--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -1,21 +1,74 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class ThemeParkTest {
 
     ThemePark themePark;
+    Attraction dodgems;
+    ArrayList<Attraction> attractions;
+    Stall stall1;
+    Stall stall2;
+    ArrayList<Stall> stalls;
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
+    ArrayList<Visitor> visitors;
 
     @Before
     public void before() {
-        themePark = new ThemePark("Wonderland");
-
+        dodgems = new Dodgems("Crash!", 8);
+        attractions = new ArrayList<Attraction>();
+        attractions.add(dodgems);
+        stall1 = new IceCreamStall("Walls", "Mr Wall", 456, 8);
+        stall2 = new TobaccoStall("Puffaway", "Benson & Hedges", 345, 1);
+        stalls = new ArrayList<Stall>();
+        stalls.add(stall1);
+        stalls.add(stall2);
+        visitor1 = new Visitor(5, 120, 90.0);
+        visitor2 = new Visitor(20, 150, 80.0);
+        visitor3 = new Visitor(58, 180, 290.0);
+        visitors = new ArrayList<Visitor>();
+        visitors.add(visitor1);
+        visitors.add(visitor2);
+        visitors.add(visitor3);
+        themePark = new ThemePark("Wonderland", attractions, stalls, visitors);
     }
 
     @Test
     public void canGetName() {
         assertEquals("Wonderland", themePark.getName());
     }
+
+    @Test
+    public void canGetAttractions() {
+        assertEquals(1, themePark.getAttractions().size());
+        assertEquals(1, themePark.getAttractionsCount());
+    }
+
+    @Test
+    public void canGetStalls() {
+        assertEquals(2, themePark.getStalls().size());
+        assertEquals(2, themePark.getStallsCount());
+    }
+
+    @Test
+    public void canGetVisitors() {
+        assertEquals(3, themePark.getVisitors().size());
+        assertEquals(3, themePark.getVisitorsCount());
+    }
+
+    @Test
+    public void canGetAllReviewed() {
+        assertEquals(3, themePark.getAllReviewed().size());
+    }
+
+//    @Test
+//    public void canVisit() {
+//
+//    }
 
 }
